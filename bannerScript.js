f23944839008af526059d008fcc1eb5e80ea5f39
mgigman <mgigman@gmail.com>
2016-04-27 00:50:53 -0400

    banner add on bottom

---
+++ bannerScript.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+				if ($('#34').length > 0) {
+				var congrat = document.getElementById("34").href = "congrats.php?id=34";
+				} 
+				if ($('#55').length > 0) {
+				var congrat = document.getElementById("55").href = "congrats.php?id=55";
+				}
+				if ($('#85').length > 0) {
+				var congrat = document.getElementById("85").href = "congrats.php?id=85";
+				}
+			});
